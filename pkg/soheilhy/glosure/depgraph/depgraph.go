9310a53b554f0cf79d1f8ad5938aa87cd37fa363
soulteary <soulteary@gmail.com>
2023-06-30 13:00:11 +0800

    chore: format

--- pkg/soheilhy/glosure/depgraph/depgraph.go
+++ pkg/soheilhy/glosure/depgraph/depgraph.go
@@ -15,122 +15,120 @@
 package depgraph
 
 import (
-  "container/list"
-  "errors"
+	"container/list"
+	"errors"
 )
 
 type DependencyGraph struct {
-  Nodes map[string]*Node
+	Nodes map[string]*Node
 }
 
 func New() DependencyGraph {
-  return DependencyGraph{make(map[string]*Node)}
+	return DependencyGraph{make(map[string]*Node)}
 }
 
 func (g *DependencyGraph) AddFile(pkg string, path string) {
-  g.Nodes[pkg] = &Node{pkg, path, list.New()}
+	g.Nodes[pkg] = &Node{pkg, path, list.New()}
 }
 
 func (g *DependencyGraph) AddDependency(from string, to string) error {
-  fromNode, ok := g.Nodes[from]
-  if !ok {
-    return errors.New("Package not found: " + from)
-  }
+	fromNode, ok := g.Nodes[from]
+	if !ok {
+		return errors.New("Package not found: " + from)
+	}
 
-  toNode, ok := g.Nodes[to]
-  if !ok {
-    return errors.New("Package not found: " + to)
-  }
+	toNode, ok := g.Nodes[to]
+	if !ok {
+		return errors.New("Package not found: " + to)
+	}
 
-  if toNode.isRecursivelyDependentOn(from) {
-    return errors.New("Circular dependency between " + from + " and " + to)
-  }
+	if toNode.isRecursivelyDependentOn(from) {
+		return errors.New("Circular dependency between " + from + " and " + to)
+	}
 
-  fromNode.Dependencies.PushBack(toNode)
-  return nil
+	fromNode.Dependencies.PushBack(toNode)
+	return nil
 }
 
 func (g *DependencyGraph) GetDependenciesOfPackage(pkg string) []*Node {
-  node, ok := g.Nodes[pkg]
-  if !ok {
-    return nil
-  }
+	node, ok := g.Nodes[pkg]
+	if !ok {
+		return nil
+	}
 
-  return g.GetDependencies([]*Node{node})
+	return g.GetDependencies([]*Node{node})
 }
 
 func (g *DependencyGraph) GetDependencies(nodes []*Node) []*Node {
-  return g.getDependencies(nodes, make([]*Node, 0))
+	return g.getDependencies(nodes, make([]*Node, 0))
 }
 
 func (g *DependencyGraph) getDependencies(nodes []*Node, deps []*Node) []*Node {
-  for _, node := range nodes {
-    if contains(deps, node) {
-      continue
-    }
+	for _, node := range nodes {
+		if contains(deps, node) {
+			continue
+		}
 
-    for e := node.Dependencies.Front(); e != nil; e = e.Next() {
-      eDeps := g.getDependencies([]*Node{e.Value.(*Node)}, deps)
-      for _, dependencyNode := range eDeps {
-        if contains(deps, dependencyNode) {
-          continue
-        }
-        deps = append(deps, dependencyNode)
-      }
-    }
-    deps = append(deps, node)
-  }
-  return deps
+		for e := node.Dependencies.Front(); e != nil; e = e.Next() {
+			eDeps := g.getDependencies([]*Node{e.Value.(*Node)}, deps)
+			for _, dependencyNode := range eDeps {
+				if contains(deps, dependencyNode) {
+					continue
+				}
+				deps = append(deps, dependencyNode)
+			}
+		}
+		deps = append(deps, node)
+	}
+	return deps
 }
 
 type Node struct {
-  Pkg string
-  Path string
-  Dependencies *list.List
+	Pkg          string
+	Path         string
+	Dependencies *list.List
 }
 
 func contains(nodes []*Node, newNode *Node) bool {
-  for _, node := range nodes {
-    if node.Path == newNode.Path {
-      return true
-    }
-  }
+	for _, node := range nodes {
+		if node.Path == newNode.Path {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
 func containsPkg(nodes []*Node, pkg string) bool {
-  for _, node := range nodes {
-    if node.Pkg == pkg {
-      return true
-    }
-  }
+	for _, node := range nodes {
+		if node.Pkg == pkg {
+			return true
+		}
+	}
 
-  return false
+	return false
 }
 
-
 func (n *Node) isRecursivelyDependentOn(pkg string) bool {
-  return n.isRecursivelyDependentOnWithCache(pkg, make([]*Node, 0))
+	return n.isRecursivelyDependentOnWithCache(pkg, make([]*Node, 0))
 }
 
 func (n *Node) isRecursivelyDependentOnWithCache(pkg string,
-                                                 checked []*Node) bool {
-  if pkg == n.Pkg {
-    return true;
-  }
+	checked []*Node) bool {
+	if pkg == n.Pkg {
+		return true
+	}
 
-  if containsPkg(checked, pkg) {
-    return false;
-  }
+	if containsPkg(checked, pkg) {
+		return false
+	}
 
-  for e := n.Dependencies.Front(); e != nil; e = e.Next() {
-    if e.Value.(*Node).isRecursivelyDependentOnWithCache(pkg, checked) {
-      return true
-    }
-  }
+	for e := n.Dependencies.Front(); e != nil; e = e.Next() {
+		if e.Value.(*Node).isRecursivelyDependentOnWithCache(pkg, checked) {
+			return true
+		}
+	}
 
-  checked = append(checked, n)
-  return false
+	checked = append(checked, n)
+	return false
 }
-
--- pkg/soheilhy/glosure/glosure_test.go
+++ pkg/soheilhy/glosure/glosure_test.go
@@ -15,94 +15,93 @@
 package glosure
 
 import (
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "testing"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"testing"
 )
 
 func TestDialClosureApi(t *testing.T) {
-  cc := NewCompiler(".")
-  res, err := cc.dialClosureApi("var i = 0; i += 1; window.alert(1)", "")
-  if err != nil {
-    t.Error("Error(s) in closure api call: ", res.Errors)
-  }
+	cc := NewCompiler(".")
+	res, err := cc.dialClosureApi("var i = 0; i += 1; window.alert(1)", "")
+	if err != nil {
+		t.Error("Error(s) in closure api call: ", res.Errors)
+	}
 
-  if len(res.CompiledCode) == 0 {
-    t.Error("Got empty results from the closure compiler.")
-  }
+	if len(res.CompiledCode) == 0 {
+		t.Error("Got empty results from the closure compiler.")
+	}
 }
 
 func TestGetClosureDependecies(t *testing.T) {
-  deps, err := getClosureDependecies("./test_resources/pkg1.js")
-  if err != nil {
-    t.Error(err)
-    return
-  }
-  if deps == nil || deps[0] != "pkg2" || deps[1] != "pkg3" {
-    t.Error("Invalid dependecies loaded from javascript file: ", deps)
-  }
+	deps, err := getClosureDependecies("./test_resources/pkg1.js")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if deps == nil || deps[0] != "pkg2" || deps[1] != "pkg3" {
+		t.Error("Invalid dependecies loaded from javascript file: ", deps)
+	}
 }
 
 func TestGetClosurePackage(t *testing.T) {
-  pkgs, err := getClosurePackage("./test_resources/pkg1.js")
-  if err != nil {
-    t.Error(err)
-    return
-  }
+	pkgs, err := getClosurePackage("./test_resources/pkg1.js")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-  if len(pkgs) != 1 || pkgs[0] != "pkg1" {
-    t.Error("Invalid package loaded from javascript file: ", pkgs)
-  }
+	if len(pkgs) != 1 || pkgs[0] != "pkg1" {
+		t.Error("Invalid package loaded from javascript file: ", pkgs)
+	}
 }
 
 func TestCompilerJar(t *testing.T) {
-  cc := NewCompiler("./test_resources")
-  err := cc.Compile("pkg1.min.js")
-  if err != nil {
-    t.Error(err)
-    return
-  }
+	cc := NewCompiler("./test_resources")
+	err := cc.Compile("pkg1.min.js")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-  _, err = ioutil.ReadFile("./test_resources/pkg1.min.js")
-  if err != nil {
-    t.Error(err)
-  }
+	_, err = ioutil.ReadFile("./test_resources/pkg1.min.js")
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func TestCompilerApi(t *testing.T) {
-  cc := NewCompiler("./test_resources")
-  cc.UseClosureApi = true
-  err := cc.Compile("pkg1.min.js")
-  if err != nil {
-    t.Error(err)
-    return
-  }
+	cc := NewCompiler("./test_resources")
+	cc.UseClosureApi = true
+	err := cc.Compile("pkg1.min.js")
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
-  _, err = ioutil.ReadFile("./test_resources/pkg1.min.js")
-  if err != nil {
-    t.Error(err)
-  }
+	_, err = ioutil.ReadFile("./test_resources/pkg1.min.js")
+	if err != nil {
+		t.Error(err)
+	}
 }
 
 func Example() {
-  // Parse the flags if you want to use glog.
-  flag.Parse()
+	// Parse the flags if you want to use glog.
+	flag.Parse()
 
-  // Creat a new compiler assuming javascript files are in "example/js".
-  cc := NewCompiler("./example/js/")
-  // Use strict mode for the closure compiler. All warnings are treated as
-  // error.
-  cc.Strict()
-  // Or use debug mode.
-  // cc.Debug()
+	// Creat a new compiler assuming javascript files are in "example/js".
+	cc := NewCompiler("./example/js/")
+	// Use strict mode for the closure compiler. All warnings are treated as
+	// error.
+	cc.Strict()
+	// Or use debug mode.
+	// cc.Debug()
 
-  // Use advanced optimizations.
-  cc.CompilationLevel = AdvancedOptimizations
+	// Use advanced optimizations.
+	cc.CompilationLevel = AdvancedOptimizations
 
-  http.Handle("/", GlosureServer(cc))
-  fmt.Println("Checkout http://localhost:8080/sample.min.js")
-  http.ListenAndServe(":8080", nil);
+	http.Handle("/", GlosureServer(cc))
+	fmt.Println("Checkout http://localhost:8080/sample.min.js")
+	http.ListenAndServe(":8080", nil)
 }
-
